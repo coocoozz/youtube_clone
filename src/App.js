a9f2d4166ef8268a657ee2f7a05775fafe36f7f2
kkwang <coocoozz@naver.com>
2021-02-24 00:03:28 +0900

    show video list with title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import VideoList from "./components/video_list/video_list";
+
+//const auth = "AIzaSyDno7uebxqEj2tkvcmVj2Xkm8hpn27oGAI";
+//const baseUrl = "https://youtube.googleapis.com/youtube/v3";
+const baseUrl = "http://127.0.0.1:8080";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(`${baseUrl}/videos`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
